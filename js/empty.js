6fa67b6ae35b3459b7eed83e757ffe588c948a69
singlewindy <singlewindy@gmail.com>
2013-08-25 15:03:54 +0800

    improved the sidebar effect

--- js/empty.js
+++ js/empty.js
@@ -2,12 +2,13 @@
         var $w = $(window);
         var $nav = $("#navigation");
         var $emp = $('#empty');
+        var $sid = $('#sidebar')
         
 
         $w.scroll(function() {
             $nav[$w.scrollTop() > 106 ? "addClass" : "removeClass"]("fixed");
             $emp[$w.scrollTop() > 106 ? "addClass" : "removeClass"]("fixed");
+            $sid[$w.scrollTop() > 106 ? "addClass" : "removeClass"]("fixed");
         });
-
 
 })();
